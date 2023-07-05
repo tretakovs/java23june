a163c715381de417cff93aa2d07d8676e234e209
tretakovs <tretakovs960@gmail.com>
2023-07-05 19:41:49 +0600

    решил часть2 задача1

---
+++ src/pr2/task1.java
@@ -0,0 +1,17 @@
+package pr2;
+
+import java.util.Scanner;
+
+public class task1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        double x;
+        //
+        x = s.nextDouble();
+        double result;
+
+        result = x + 7;
+        //
+        System.out.printf("%.4f", result);
+    }
+}
