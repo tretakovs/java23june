8b4ffabbeab205859e273d28c175ed60b3187168
tretakovs <tretakovs960@gmail.com>
2023-07-05 18:51:05 +0600

    решил задачу 1

--- src/pr1/task1.java
+++ src/pr1/task1.java
@@ -1,4 +1,16 @@
 package pr1;
 
 public class task1 {
+    public static void main(String[] args) {
+        System.out.println("+---+");
+        System.out.println("|   |");
+        System.out.println("|   |");
+        System.out.println("+---+");
+
+
+
+    }
+
 }
+    git config --global user.email "you@example.com"
+        git config --global user.name "Your Name"
