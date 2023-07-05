e6c5034b69b80715d47274b28bd189a379f09cf6
tretakovs <tretakovs960@gmail.com>
2023-07-05 19:21:52 +0600

    решил задачу 3

---
+++ src/pr1/task12.java
@@ -0,0 +1,10 @@
+package pr1;
+import java.util.Scanner;
+
+public class task12 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String line = s.nextLine();
+
+    }
+}
