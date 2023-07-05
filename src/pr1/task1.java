d5ce2d86f5c51b5ad6cb9e8344c036ac0e80dbbe
tretakovs <tretakovs960@gmail.com>
2023-07-05 19:05:07 +0600

    задача 2

--- src/pr1/task1.java
+++ src/pr1/task1.java
@@ -12,5 +12,3 @@ public class task1 {
     }
 
 }
-    git config --global user.email "you@example.com"
-        git config --global user.name "Your Name"
---
+++ src/pr1/task2.java
@@ -0,0 +1,10 @@
+package pr1;
+
+public class task2 {
+    public static void main(String[] args) {
+        System.out.println("#");
+        System.out.println("##");
+        System.out.println("###");
+        System.out.println("####");
+    }
+}
